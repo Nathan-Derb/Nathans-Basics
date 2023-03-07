26e1e87f09b47a019e01f6ceea9f17d8cc56948e
Nathan-Derb <121068261+Nathan-Derb@users.noreply.github.com>
2023-03-06 19:59:51 -0800

    Intro to JS - Weather

---
+++ weather.js
@@ -0,0 +1,23 @@
+const companyName = "JsWeather";
+
+let isSunny = "Sunny";
+isSunny = "Overcast";
+
+let isRaining = "Raining";
+if (isRaining) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+let windSpeed = 0;
+windSpeed += 14;
+windSpeed -= 4;
+
+let windDirection = "North";
+windDirection = "South East";
+
+const dayOfWeek = getDay();
+
+let expectedPrecipitation = 0;
+expectedPercipitation += 2.2;
