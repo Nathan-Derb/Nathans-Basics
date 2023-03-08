7068fb2765557b2e10ccb7a600098657b81536b3
Nathan-Derb <121068261+Nathan-Derb@users.noreply.github.com>
2023-03-07 16:33:59 -0800

    Add files via upload

---
+++ car.js
@@ -0,0 +1,10 @@
+let car = "KodeCar";
+if ("dark") {
+  console.log("lights on");
+} else if ("raining") {
+  console.log("wipers on");
+} else if ("hazard ahead") {
+  console.log("reroute");
+} else {
+  console.log("proceed driving");
+}
